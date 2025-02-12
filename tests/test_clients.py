9b9b79e56965570c2b4b623526d3e5eba4f0a281
Hillel Arnold <hillel.arnold@gmail.com>
2025-02-12 13:54:50 -0500

    add additional tests to clients

--- tests/test_clients.py
+++ tests/test_clients.py
@@ -155,8 +155,6 @@ class ArchivesSpaceClientTests(TestCase):
 
 class AuroraClientTests(TestCase):
 
-    # TODO add tests for strip_url and get
-
     @patch('electronbonder.client.ElectronBond.authorize_oauth')
     def setUp(self, mock_authorize):
         mock_authorize.return_value = True
@@ -180,6 +178,13 @@ class AuroraClientTests(TestCase):
         self.assertEqual(self.client.client.config['oauth_client_id'], self.oauth_client_id)
         self.assertEqual(self.client.client.config['oauth_client_secret'], self.oauth_client_secret)
 
+    def test_strip_url(self):
+        """Asserts URL construction."""
+        input = "https://example.org/accessions/1"
+        expected = "/accessions/1/"
+        output = self.client.strip_url(input)
+        self.assertEqual(output, expected)
+
     @patch('electronbonder.client.ElectronBond.patch')
     def test_update(self, mock_patch):
         """Asserts URL construction and exception handling."""
@@ -197,6 +202,23 @@ class AuroraClientTests(TestCase):
         mock_patch.return_value = MockResponse({}, 404, text="Not Found")
         with self.assertRaises(AuroraClientError) as err:
             self.client.update("https://example.org/accessions/1", data)
+        self.assertEqual(str(err.exception), "Error sending request /accessions/1/ to Aurora: 404 Not Found")
+
+    @patch('electronbonder.client.ElectronBond.get')
+    def test_get(self, mock_get):
+        """Asserts URL construction and exception handling."""
+        mock_get.return_value = MockResponse({}, 200)
+
+        output = self.client.get("https://example.org/accessions/1")
+        self.assertEqual(output, {})
+        mock_get.assert_called_once_with(
+            '/accessions/1/',
+            headers={'Content-Type': 'application/json'}
+        )
+
+        mock_get.return_value = MockResponse({}, 404, text="Not Found")
+        with self.assertRaises(AuroraClientError) as err:
+            self.client.get("https://example.org/accessions/1")
         self.assertEqual(str(err.exception), "Error sending request /accessions/1/ to Aurora: 404 Not Found")
 
 
